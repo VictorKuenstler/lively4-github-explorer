6e6c522792bf28443fce9a91320c451c7fe37074
Friedrich Schöne <friedrich.schoene@gmx.de>
2018-10-31 20:37:08 +0100

    added database connection to server

--- server/controller.py
+++ server/controller.py
@@ -1,8 +1,15 @@
+from psycopg2.sql import SQL
 import responder
 
+from server.postgres.postgres_db import PostgresDbDictCursor
 
 api = responder.API()
 
 @api.route('/test')
 def test(req, resp):
-    resp.text = 'Test String!\n'
+    with PostgresDbDictCursor() as cursor:
+        query = SQL('SELECT * FROM table_sizes')
+        cursor.execute(query)
+        response = [dict(row) for row in cursor]
+
+    resp.media = response
