2f66713725862fd03a4b7f355c66703151503bed
Friedrich Schöne <friedrich.schoene@gmx.de>
2018-11-10 12:48:25 +0100

    added models for alle tables

--- server/models.py
+++ server/models.py
@@ -1,15 +1,15 @@
 import os
-from peewee import Model, PostgresqlDatabase, TextField, ForeignKeyField
+from peewee import CompositeKey, ForeignKeyField, IntegerField, Model, PostgresqlDatabase, TextField
 
-dbname = os.environ['PSQL_NAME']
+db_name = os.environ['PSQL_NAME']
 user = os.environ['PSQL_USER']
 password = os.environ['PSQL_PASSWORD']
 
-db = PostgresqlDatabase(dbname, user=user, password=password)
+db = PostgresqlDatabase(db_name, user=user, password=password)
 
 
 class User(Model):
-    login = TextField(unique=True, index=True)
+    login = TextField(unique=True)
     company = TextField()
     state = TextField()
     city = TextField()
@@ -21,11 +21,54 @@ class User(Model):
 
 
 class Project(Model):
-    url = TextField()
+    url = TextField(unique=True)
     owner = ForeignKeyField(User, backref='projects')
     name = TextField()
     description = TextField()
+    forked_from = ForeignKeyField('self', column_name='forked_from', backref='forks')
 
     class Meta:
         database = db
         table_name = 'projects'
+
+
+class Commit(Model):
+    sha = TextField(unique=True)
+    author = ForeignKeyField(User, backref='authored_commits')
+    committer = ForeignKeyField(User, backref='commits')
+
+    class Meta:
+        database = db
+        table_name = 'commits'
+
+
+class ProjectCommit(Model):
+    project = ForeignKeyField(Project, backref='project_commits')
+    commit = ForeignKeyField(Commit, backref='project_commits')
+
+    class Meta:
+        database = db
+        table_name = 'project_commits'
+        primary_key = CompositeKey('project', 'commit')
+
+
+class CommitRelationship(Model):
+    parent = ForeignKeyField(Commit, backref='children')
+    child = ForeignKeyField(Commit, column_name='commit_id', backref='parents')
+
+    class Meta:
+        database = db
+        table_name = 'commit_parents'
+        primary_key = CompositeKey('parent', 'child')
+
+
+class Comment(Model):
+    commit = ForeignKeyField(Commit, backref='comments')
+    author = ForeignKeyField(User, column_name='user_id', backref='comments')
+    body = TextField()
+    line = IntegerField()
+    position = IntegerField()
+
+    class Meta:
+        database = db
+        table_name = 'commit_comments'
