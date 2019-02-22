b6996a0c0880007562a0ac5c2f483cc2ecff1bae
Friedrich Schöne <friedrich.schoene@gmx.de>
2019-02-22 15:39:27 +0100

    renamed controller file

--- __main__.py
+++ __main__.py
@@ -3,7 +3,7 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
-from server.controller import api
+from server.views import api
 
 if __name__ == '__main__':
     api.run(port=os.environ['PORT'])
